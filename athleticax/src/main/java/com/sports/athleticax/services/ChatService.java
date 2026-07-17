e4ca9f9f412ea729b14b6178e5b9598da57df783
ANEESH DAS <155869164+LearnCodeAndRun@users.noreply.github.com>
2026-07-17 10:24:58 +0530

    Add import for File class in ChatService

--- athleticax/src/main/java/com/sports/athleticax/services/ChatService.java
+++ athleticax/src/main/java/com/sports/athleticax/services/ChatService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.io.File;
 import java.util.List;
 
 @Service
