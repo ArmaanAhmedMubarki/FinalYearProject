f81a90247dc136f7b307b9b4994c0f09c624c16c
ANEESH DAS <155869164+LearnCodeAndRun@users.noreply.github.com>
2026-07-19 10:12:06 +0530

    Add package and imports to NewsService

--- athleticax/src/main/java/com/sports/athleticax/services/NewsService.java
+++ athleticax/src/main/java/com/sports/athleticax/services/NewsService.java
@@ -1,3 +1,8 @@
+package com.sports.athleticax.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 @Service
 public class NewsService {
 
