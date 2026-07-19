3101fe0bd0561ce584ee2bf874378f4656ed6732
ANEESH DAS <155869164+LearnCodeAndRun@users.noreply.github.com>
2026-07-19 10:12:35 +0530

    Enable CORS for NewsController
    Add CrossOrigin annotation to allow requests from a specific origin.

--- athleticax/src/main/java/com/sports/athleticax/controller/NewsController.java
+++ athleticax/src/main/java/com/sports/athleticax/controller/NewsController.java
@@ -1,3 +1,13 @@
+package com.sports.athleticax.controller;
+
+import com.sports.athleticax.services.NewsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/news")
 @CrossOrigin(origins = "https://resilient-centaur-8cbadb.netlify.app")
